83229bd16231781be566dddb1c787f9e55b72433
vagrant <asucarlos>
2019-06-04 22:45:25 +0000

    created taking off the longURL(skipped creating randomstring functioin part)

--- express_server.js
+++ express_server.js
@@ -1,4 +1,4 @@
-function generateRandomString() {
+// function generateRandomString() {
 
 const express = require("express");
 const app = express();
@@ -6,17 +6,20 @@ const PORT = 8080;
 const bodyParser = require("body-parser");
 app.use(bodyParser.urlencoded({extended: true}));
 
+let urlDatabase = {
+  "b2xVn2": "http://www.lighthouselabs.ca",
+  "9sm5xK": "http://www.google.com"
+};
+
 app.post("/urls", (req, res) => {
-  console.log(req.body);  // Log the POST request body to the console
+  urlDatabase.shortURL = req.body.longURL;
+  console.log(urlDatabase); // Log the POST request body to the console
   res.send("Ok");         // Respond with 'Ok' (we will replace this)
 });
 
 app.set("view engine", "ejs");
 
-let urlDatabase = {
-  "b2xVn2": "http://www.lighthouselabs.ca",
-  "9sm5xK": "http://www.google.com"
-};
+
 
 
 app.get("/urls/new", (req, res) => {
@@ -51,4 +54,4 @@ app.listen(PORT, () => {
   console.log(`Example app listening on port ${PORT}!`);
 })
 
-}
+// }
