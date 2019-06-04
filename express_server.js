2ccec890518384519bb254a08435e426f0d347f4
vagrant <asucarlos>
2019-06-04 18:52:34 +0000

    added for loop to show urls

--- express_server.js
+++ express_server.js
@@ -2,15 +2,23 @@ const express = require("express");
 const app = express();
 const PORT = 8080;
 
-app.set("view engine", "ejs")
+app.set("view engine", "ejs");
 
-const urlDatabase = {
+let urlDatabase = {
   "b2xVn2": "http://www.lighthouselabs.ca",
   "9sm5xK": "http://www.google.com"
 };
 
+
+
 app.get("/urls.json", (req, res) => {
   res.json(urlDatabase);
+})
+
+app.get("/urls", (req, res) => {
+  let templateVars = {urls: urlDatabase};
+  res.render("urls_index", templateVars)
+  // console.log(req)
 })
 
 app.get("/", (req, res) => {
