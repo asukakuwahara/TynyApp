b6ae948a99e58a3034976c5d1050e46a2150644f
vagrant <kuwahara.mee@gmail.com>
2019-06-11 00:06:28 +0000

    helper function module created

---
+++ helpers.js
@@ -0,0 +1,24 @@
+//create random string for user id and short URLs
+module.exports = {
+
+generateRandomString: function() {
+  return Math.random().toString(36).substring(2, 8);
+},
+
+emailLookup: function(email, users){
+  for (user in users) {
+    if (email === users[user].email) {
+      return user;
+    }
+  }
+},
+
+urlsForUser: function(id, data){
+  for (url in urlDatabase) {
+    if (id === urlDatabase[url].userID) {
+      return urlDatabase[url].longURL;
+    }
+  }
+}
+
+};
